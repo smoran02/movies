38103b9359f8ae6329ba9589385faaa0cbaf95d8
Spencer Moran <smoran02@gmail.com>
2013-08-02 19:52:58 -0700

    Add mongodb to heroku

--- server.js
+++ server.js
@@ -37,11 +37,14 @@ app.get( '/api', function( request, response ) {
 	response.send( 'Movie api is running' );
 });
 
-if (err) { 
- console.log ('ERROR connecting to: ' + uristring + '. ' + err);
- } else {
- console.log ('Succeeded connected to: ' + uristring);
- }
+mongoose.connect(uristring, function (err, res) {
+	if (err) { 
+		console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+	} else {
+		console.log ('Succeeded connected to: ' + uristring);
+	}
+	
+});
 
 var Genres = new mongoose.Schema({
 	genre: String
